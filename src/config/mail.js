2dea037a0eee94cb425abd56571a15bdc537cf21
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2020-02-01 19:29:48 -0300

    nodemailer config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '91f319a90ac52c',
+        pass: 'f8626d71373e2d',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
